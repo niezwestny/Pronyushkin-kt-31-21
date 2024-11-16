6c0081fecc5d9f45e37d4d77e61e044ac7d6b8d3
Александр Пронюшкин <niezwestny@mail.ru>
2024-11-16 11:35:46 +0300

    fix DisciplineLoadFilter

--- Proekt_Prakt_Lab/Filters/DisciplineLoadFilter.cs
+++ Proekt_Prakt_Lab/Filters/DisciplineLoadFilter.cs
@@ -2,7 +2,7 @@
 {
     public class DisciplineLoadFilter
     {
-        public int Discipline_Load_ID { get; set;}
+        
         public int Min { get; set; }
         public int Max { get; set; }    
     }
